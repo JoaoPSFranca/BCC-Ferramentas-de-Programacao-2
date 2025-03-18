7f09a8d73466fff1d76255c46d5fd3cf248f3a7b
JoaoPSFranca <francasjoaopedro@gmail.com>
2025-03-18 10:49:57 -0300

    agora foi

--- Spring/estoque_V3/src/main/java/br/edu/ifsp/pep/bcc/estoque/exceptions/ClientNotFoundException.java
+++ Spring/estoque_V3/src/main/java/br/edu/ifsp/pep/bcc/estoque/exceptions/ClientNotFoundException.java
@@ -3,6 +3,6 @@ package br.edu.ifsp.pep.bcc.estoque.exceptions;
 public class ClientNotFoundException extends RuntimeException {
     public ClientNotFoundException() {
         super("Client not found");
-        // Apenas testando
+        // Agora vai
     }
 }
